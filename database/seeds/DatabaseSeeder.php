5c8d34d90ac7d16973b6229784319287fdb0c92b
Alejandro Nuñez <39980306+AlexxNu@users.noreply.github.com>
2020-09-14 21:42:13 -0500

    Creacion de factorias/faker para BD

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\User;
+use App\Product;
+use App\Category;
+use App\Transaction;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +16,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+
+        Category::truncate();
+        Product::truncate();
+        Transaction::truncate();
+        User::truncate();
+        DB::table('category_product')->truncate();
+
+        $cantidadUsuarios = 200;
+        $cantidadCategorias = 30;
+        $cantidadProductos = 1000;
+        $cantidadTransacciones = 1000;
+
+        factory(User::class,$cantidadUsuarios)->create();
+        factory(Category::class,$cantidadUsuarios)->create();
+        
+        factory(Product::class,$cantidadProductos)->create()->each(
+            function($producto){
+                $cateogiras = Category::all()->random(mt_rand(1,5))->pluck('id');
+
+                $producto->categories()->attach($cateogiras);
+            }
+        );
+        
+        factory(Transaction::class,$cantidadTransacciones)->create();
+        
     }
 }
