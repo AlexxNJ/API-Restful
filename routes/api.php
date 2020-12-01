43674f324817d74b1623a3c7c1e9d85270852898
Alejandro Nuñez <39980306+AlexxNu@users.noreply.github.com>
2020-12-01 14:09:01 -0600

    Implementando las operaciones complejas para Product

--- routes/api.php
+++ routes/api.php
@@ -32,6 +32,10 @@ use Illuminate\Http\Request;
  Route::resource('categories.transactions', 'Category\CategoryTransactionController',['only'=>['index']]);
  /* Products */
  Route::resource('products', 'Product\ProductController',['only'=>['index','show']]);
+ Route::resource('products.transactions', 'Product\ProductTransactionController',['only'=>['index']]);
+ Route::resource('products.buyers.transactions', 'Product\ProductBuyerTransactionController',['only'=>['store']]);
+ Route::resource('products.buyers', 'Product\ProductBuyerController',['only'=>['index']]);
+ Route::resource('products.categories', 'Product\ProductCategoryController',['only'=>['index','update','destroy']]);
  /* Sellers */
  Route::resource('sellers', 'Seller\SellerController',['only'=>['index','show']]);
  Route::resource('sellers.transactions', 'Seller\SellerTransactionController',['only'=>['index']]);
